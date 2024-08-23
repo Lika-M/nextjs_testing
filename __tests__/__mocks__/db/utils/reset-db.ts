b1251dd69615eeefabb9c8c564528d7685600ee7
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-08-23 11:46:03 +0300

    Created necessary test database files and added functionality to reset the test database before each test run.

---
+++ __tests__/__mocks__/db/utils/reset-db.ts
@@ -0,0 +1,21 @@
+import { readFakeData } from "@/__tests__/__mocks__/fakeData";
+import { filenames, writeJSONToFile } from "@/lib/db/db-utils";
+
+export async function resetDB() {
+  //failsafe against production db!
+  const isSafeToReset = process.env.NODE_ENV === "test";
+
+  if (!isSafeToReset) {
+    console.log("WARNING: Database rest is unavailable outside test environment!");
+    return;
+  }
+
+  const { fakeBands, fakeShows, fakeReservations, fakeUsers } = await readFakeData();
+  // overwrite data in files
+  await Promise.all([
+    writeJSONToFile(filenames.bands, fakeBands),
+    writeJSONToFile(filenames.shows, fakeShows),
+    writeJSONToFile(filenames.reservations, fakeReservations),
+    writeJSONToFile(filenames.users, fakeUsers)
+  ]);
+}
