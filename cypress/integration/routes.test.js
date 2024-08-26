f252ae2c9d10a88a05f73ba0e08bd36bbe00773a
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-08-26 15:32:37 +0300

    Added tests against some other static routes.

--- cypress/integration/routes.test.js
+++ cypress/integration/routes.test.js
@@ -1,5 +1,23 @@
-it("displays correct heading", () => {
-  cy.visit("/");
-  cy.findByRole("button", {name: /shows/i}).click();
-  cy.findByRole("heading", {name: /upcoming shows/i}).should("exist");
+// test static routes
+describe("Testing static routes", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("displays correct heading when navigate to /shows", () => {
+    cy.findByRole("button", { name: /shows/i }).click();
+    cy.findByRole("heading", { name: /upcoming shows/i }).should("exist");
+  });
+  
+  it("displays correct heading when navigate to /bands", () => {
+    cy.findByRole("button", { name: /bands/i }).click();
+    cy.findByRole("heading", { name: /Our Illustrious Performers/i }).should("exist");
+  });
+  
+  it("displays correct heading when navigate to /user", () => {
+    cy.findByRole("button", { name: /sign in/i }).click();
+    cy.findByRole("heading", { name: /Sign in to your account/i }).should("exist");
+  });
 })
+
+//test dynamic routes
