db947d0331d1d6ea838abaa47de6eca82a2dd436
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-08-27 11:38:10 +0300

    Added dynamic path tests for band detail page: handling existing ID, unknown band, and band added after build time.

--- cypress/integration/routes.test.js
+++ cypress/integration/routes.test.js
@@ -1,27 +1,48 @@
+import { generateNewBand } from "../../__tests__/__mocks__/fakeData/newBand";
+import { generateRandomId } from "../../lib/features/reservations/utils";
+
 // test static routes
-describe("Testing static routes", () => {
+describe("Static routes", () => {
   beforeEach(() => {
     cy.visit("/");
   });
 
-  it("displays correct heading when navigate to /shows", () => {
+  it("displays the correct heading when navigating to /shows", () => {
     cy.findByRole("button", { name: /shows/i }).click();
     cy.findByRole("heading", { name: /upcoming shows/i }).should("exist");
   });
 
-  it("displays correct heading when navigate to /bands", () => {
+  it("displays the correct heading when navigating to /bands", () => {
     cy.findByRole("button", { name: /bands/i }).click();
     cy.findByRole("heading", { name: /Our Illustrious Performers/i }).should("exist");
   });
 
-  it("displays correct heading when navigate to /user", () => {
+  it("displays the correct heading when navigating to /user", () => {
     cy.findByRole("button", { name: /sign in/i }).click();
     cy.findByRole("heading", { name: /Sign in to your account/i }).should("exist");
   });
 });
 
-//test dynamic routes
+// Test dynamic routes
+describe("Dynamic Routes", () => {
+  beforeEach(() => {
+    cy.task("db:reset");
+  });
 
-it.only("Test db reset", () => {
-  cy.task("db:reset");
+  it("displays the correct band name for a band that existed at build time (bands/id)", () => {
+    cy.visit("/bands/1");
+    cy.findByRole("heading", { name: /Shamrock Pete/i }).should("exist");
+  });
+
+  it("displays an error for a bands/id route when the band doesn't exist", () => {
+    cy.visit("/bands/12345");
+    cy.findByText(/Error: band not found/i).should("exist");
+  });
+
+  it("displays the correct band name for a band added after build time (bands/id)", () => {
+    const bandId = generateRandomId();
+    const newBand = generateNewBand(bandId);
+    cy.task("addNewBand", newBand).visit(`/bands/${bandId}`);
+    cy.findByRole("heading", { name: /Avalanche of Cheese/i }).should("exist");
+  });
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,14 +1,15 @@
 /// <reference types="cypress" />
 const { resetDB } = require("../../__tests__/__mocks__/db/utils/reset-db");
-
+const { addBand } = require("../../lib/features/bands/queries");
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // reset db before test
   on("task", {
-    "db:reset": () => resetDB().then(() => null)
+    // reset db before test
+    "db:reset": () => resetDB().then(() => null),
+    "addNewBand": (newBand) => addBand(newBand).then(() => null)
   });
 
   // `on` is used to hook into various events Cypress emits
