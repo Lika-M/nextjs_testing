b2bee58e29f67e526fa0dc362640c454a530ae82
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-09-16 16:16:18 +0300

    Refined regex to selectively remove scripts related to /shows page during ISR cache test.

--- cypress/integration/shows-isr.test.js
+++ cypress/integration/shows-isr.test.js
@@ -2,10 +2,10 @@ describe("ISR cache test with a build time data.", () => {
   it("skip client-side bundle to confirm data from the cache", () => {
     cy.request("/shows").its("body").then(html => {
       //remove JS scripts so they don't start automatically
-      const staticHtml = html.replace(/<scripts .*?>.*?<\/scripts>/gm, "");
+      const staticHtml = html.replace(/script src="\/_next\/static\/chunks\/pages\/shows.*?>.*?<\/script>/gm, "");
       cy.state("document").write(staticHtml);
     });
-  
+
     cy.findAllByText(/2022 apr 1[567]/i).should("have.length.of", 3);
   });
 });
