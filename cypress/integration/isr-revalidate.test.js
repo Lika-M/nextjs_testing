be6160659ffc802426dab4cc5b153491218ea3aa
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-09-18 19:48:01 +0300

    Added Cypress test for on-demand ISR revalidation of shows page
    - Added test to verify shows page revalidation after adding a new show
    - Used resetDBAndIsrCache to ensure cache is cleared between tests

--- cypress/integration/isr-revalidate.test.js
+++ cypress/integration/isr-revalidate.test.js
@@ -1,7 +1,8 @@
 import { generateNewBand } from "../../__tests__/__mocks__/fakeData/newBand";
 import { generateRandomId } from "../../lib/features/reservations/utils";
+import { generateNewShow } from "../../__tests__/__mocks__/fakeData/newShow";
 
-describe("Test revalidation pages after updating.", () => {
+describe("Test revalidation pages after updating on-demand.", () => {
   it("should load refreshed bands page from cache after new band is added", () => {
     // DB reset and check that new band is not on the page
     cy.task("db:reset").visit("/bands");
@@ -23,5 +24,23 @@ describe("Test revalidation pages after updating.", () => {
 
     // reset ISR cache to the initial db condition
     cy.resetDBAndIsrCache()
+  });
+
+  it("should load refreshed shows page from cache after a new show is added", () => {
+    cy.task("db:reset").visit("/shows");
+    cy.findByRole("heading", { name: /Avalanche of Cheese/i }).should("not.exist");
+
+    const showId = generateRandomId();
+    const newShow = generateNewShow(showId);
+
+    const secret = Cypress.env("REVALIDATION_SECRET");
+    cy.request("POST", `/api/shows?secret=${secret}`, {newShow: newShow}).then(res => {
+      expect(res.body.revalidated).to.equal(true);
+    });
+
+    cy.reload();
+    cy.findByRole("heading", {name: /avalanche of cheese/i}).should("exist");
+
+    cy.resetDBAndIsrCache();
   });
 });
