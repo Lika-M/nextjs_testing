d12ccf31f1ccdbcb1f9c269e1565920059b1c58c
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-09-20 16:26:20 +0300

    Add Cypress test for authentication flow with invalid and valid credentials
    - Test failure scenarios with incorrect email and password inputs
    - Test successful login and redirection to protected 'user' page
    - Validate sign-in form behavior and correct access to 'purchase more tickets' option after login

--- cypress/integration/auth-wrapper.test.js
+++ cypress/integration/auth-wrapper.test.js
@@ -10,7 +10,7 @@ describe("Test auth wrapper", () => {
     // and that's no option to purchase tickets
     cy.findByRole("heading", { name: /Sign in to your account/i }).should("exist");
     cy.findByRole("button", { name: /purchase/i }).should("not.exist");
-    
+
 
     //enter valid credentials and submit the form
     cy.findByLabelText(/email address/i)
@@ -33,4 +33,45 @@ describe("Test auth wrapper", () => {
     cy.findByText(email).should("exist");
     cy.findByRole("button", { name: /sign out/i }).should("exist");
   });
+
+  
+  it.only("failure flow of visiting protected 'user' page followed by successful login", () => {
+    // Reset db and visit protected user page
+    cy.task("db:reset").visit("/user");
+    cy.findByRole("heading", { name: /Sign in to your account/i }).should("exist");
+    cy.findByRole("heading", { name: /welcome/i }).should("not.exist");
+  
+    // Attempt sign in with invalid email format and check for error message
+    cy.findByLabelText(/email address/i).clear().type("test@test@asdasd");
+    cy.findByLabelText(/password/i).clear().type(password);
+    cy.findByRole("main").within(() => {
+      cy.findByRole("button", { name: /sign in/i }).click();
+    });
+    cy.findByText(/Sign in failed/i).should("exist");
+  
+    // Attempt sign in with correct email but wrong password
+    cy.findByLabelText(/email address/i).clear().type(email);
+    cy.findByLabelText(/password/i).clear().type("asdasd");
+    cy.findByRole("main").within(() => {
+      cy.findByRole("button", { name: /sign in/i }).click();
+    });
+    cy.findByText(/Sign in failed/i).should("exist");
+  
+    // Successful login should redirect to the originally requested page '/user'
+    cy.findByLabelText(/email address/i).clear().type(email);
+    cy.findByLabelText(/password/i).clear().type(password);
+    cy.findByRole("main").within(() => {
+      cy.findByRole("button", { name: /sign in/i }).click();
+    });
+  
+    // Validate that user is redirected to the user dashboard with purchase options
+    cy.findByRole("heading", { name: /Welcome test@test.test/i }).should("exist");
+    cy.findByRole("heading", { name: /Your Tickets/i }).should("exist");
+    cy.findByRole("button", { name: /purchase more tickets/i }).should("exist");
+  
+    // Ensure user is signed in and can sign out
+    cy.findByRole("button", { name: /sign in/i }).should("not.exist");
+    cy.findByRole("button", { name: /sign out/i }).should("exist");
+  });
+  
 });
