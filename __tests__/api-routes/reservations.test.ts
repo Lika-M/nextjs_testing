21ec26791482ad9c6966183a3b86dd6da723b4ad
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-10-01 16:55:40 +0300

    Tested unauthorized request for POST /api/reservations/[reservationId] and updated mock to return false.

--- __tests__/api-routes/reservations.test.ts
+++ __tests__/api-routes/reservations.test.ts
@@ -1,9 +1,11 @@
 import { testApiHandler } from "next-test-api-route-handler";
 
+import { validateToken } from "@/lib/auth/utils";
 import reservationHandler from "@/pages/api/reservations/[reservationId]";
 import userReservationsHandler from "@/pages/api/users/[userId]/reservations";
 
 jest.mock("@/lib/auth/utils");
+const mockValidateToken = validateToken as jest.Mock;
 
 test("POST /api/reservations/[reservationId] create a reservation", async () => {
   await testApiHandler({
@@ -47,6 +49,32 @@ test("POST /api/reservations/[reservationId] create a reservation", async () => 
 
       const json = await res.json();
       expect(json.userReservations).toHaveLength(3);
+    }
+  });
+});
+
+
+test("POST api/reservations/[reservationId] return 401 when the user is not authorized", async () => {
+  // updating mock function returned value
+  mockValidateToken.mockResolvedValue(false);
+
+  await testApiHandler({
+    handler: userReservationsHandler,
+    paramsPatcher: (params) => { params["userId"] = 1 },
+    test: async ({ fetch }) => {
+      const res = await fetch({
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+          showId: 0,
+          userId: 1,
+          seatCount: 2,
+        })
+      });
+      // unauthorize
+      expect(res.status).toBe(401);
     }
   });
 });
